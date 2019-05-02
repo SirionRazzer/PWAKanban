a7cb74ad9063500aedd99cb5be9c532a39d63896
SirionRazzer <soukaltom@gmail.com>
2019-05-02 10:53:19 +0200

    Drag'n'drop is working now

--- app/app.js
+++ app/app.js
@@ -1,7 +1,14 @@
-    // Check that service workers are registered
-    if ('serviceWorker' in navigator) {
-        // Use the window load event to keep the page load performant
-        window.addEventListener('load', () => {
-            navigator.serviceWorker.register('/service-worker.js');
-        });
-    }
+// Check that service workers are registered
+if ('serviceWorker' in navigator) {
+    // Use the window load event to keep the page load performant
+    window.addEventListener('load', () => {
+        navigator.serviceWorker.register('/service-worker.js');
+    });
+}
+
+var drake = null;
+// Initialize Dragula
+window.onload = function() {
+    drake = dragula([document.getElementById('right1'), document.getElementById('right2'), document.getElementById('right3')]);
+    console.log(drake.containers);
+}
