2d386adaba8c09ff726da9eb4f7fb59394919e12
SirionRazzer <soukaltom@gmail.com>
2019-05-09 16:43:04 +0200

    Added initial position handling

--- app/app.js
+++ app/app.js
@@ -27,14 +27,24 @@ function TaskListViewModel() {
     // Operations
     self.addTask = function () {
         self.tasks.push(new Task({
-            text: this.newTaskText()
+            text: this.newTaskText(),
+            position: this.tasks().length,
+            isDone: false
         })); //, position: this.tasks.length, isDone: false 
         self.newTaskText("");
         console.log(this.tasks().length);
         hideModal();
     };
+    self.swapTasks = function (position1, position2) {
+        var tmp = self.tasks[position1];
+        self.tasks[position1] = self.tasks[position2];
+        self.tasks[position2] = tmp;
+    };
     self.removeTask = function (task) {
         self.tasks.remove(task)
+    };
+    self.removeNthTask = function (position) {
+        self.tasks.remove(self.tasks()[position]);
     };
 
     // // Load initial state from server, convert it to Task instances, then populate self.tasks
@@ -48,16 +58,9 @@ function hideModal() {
     $('#addModal').modal('hide');
 }
 
-
-// function toggleSpinner(enabled) {
-//     if (enabled) {
-//         document.getElementById("spinner").style.visibility = "visible";
-//     } else {
-//         document.getElementById("spinner").style.visibility = "invisible";
-//     }
-// } 
-
 var model = new TaskListViewModel()
+var tmpPositionStart = 0;
+var tmpPositionEnd = 0;
 ko.applyBindings(model);
 
 // Initialize Dragula
@@ -65,13 +68,38 @@ var drake = null
 window.onload = function () {
     drake = dragula([document.getElementById('list1')], {
         removeOnSpill: true
-    }).on('remove', function (el) {
+    }).on('remove', function (el, container, source) {
         //el.className += ' ex-moved';
         console.log("remove");
-        console.log(model.tasks().length);
-    }).on('drop', function () {
+        console.log("from position:" + elementPosition(source));
+        console.log("tasks count:" + model.tasks().length);
+    }).on('drag', function (el, source) {
+        //el.className += ' ex-moved';
+        console.log("drag");
+        console.log("from position:" + elementPosition(el));
+        tmpPositionStart = elementPosition(el);
+    }).on('drop', function (el) {
         console.log("drop");
-        console.log(model.tasks().length);
+        console.log("to position:" + elementPosition(el));
+        tmpPositionEnd = elementPosition(el);
+        if (tmpPositionStart != tmpPositionEnd) {
+            model.swapTasks(tmpPositionStart, tmpPositionEnd);
+        }
     });
     console.log(drake.containers);
+}
+
+function elementPosition(element) {
+    if (element == null) return 0;
+    if (element.parentNode == null) return 0;
+
+    var position = 0;
+    var children = element.parentNode.children;
+    let array = [...children];
+    console.log("childNodes: " + array.length);
+    for (item of children) {
+        if (element === item) break;
+        position += 1;
+    }
+    return position;
 }
