edbd276e5fa95b8bc2e3a02e11a3c5b9d9a09378
Tomas Soukal <soukaltom@gmail.com>
2019-08-01 01:55:13 +0200

    Design update, functionality fix

--- app.js
+++ app.js
@@ -5,23 +5,68 @@ if ('serviceWorker' in navigator) {
     });
 }
 
+var savingLocked = true;
+
 var Task = function (name) {
     this.name = ko.observable(name);
+    this.name.subscribe(function () {
+        model.save();
+        console.log('item change');
+    });
 }
-
 
 var ViewModel = function () {
     // Data
     var self = this;
+    self.save = function () {
+        if (!savingLocked) {
+            var todoTasks = ko.toJSON({
+                tasks: self.todoTasks
+            });
+            var doingTasks = ko.toJSON({
+                tasks: self.doingTasks
+            });
+            var doneTasks = ko.toJSON({
+                tasks: self.doneTasks
+            });
+
+            localStorage.setItem(
+                "todoTasks",
+                todoTasks
+            );
+
+            localStorage.setItem(
+                "doingTasks",
+                doingTasks
+            );
+
+            localStorage.setItem(
+                "doneTasks",
+                doneTasks
+            );
+        }
+    }
     //self.listName = ko.observable();
     self.todoTasks = ko.observableArray([]);
     self.todoTasks.id = "todo";
+    self.todoTasks.subscribe(function () {
+        self.save();
+        console.log('todo list change');
+    });
 
     self.doingTasks = ko.observableArray([]);
     self.doingTasks.id = "doing";
+    self.doingTasks.subscribe(function () {
+        self.save();
+        console.log('doing list change');
+    });
 
     self.doneTasks = ko.observableArray([]);
     self.doingTasks.id = "done";
+    self.doneTasks.subscribe(function () {
+        self.save();
+        console.log('done list change');
+    });
 
     self.selectedTask = ko.observable();
 
@@ -37,69 +82,54 @@ var ViewModel = function () {
         }
     };
 
+    self.clear = function () {
+        self.todoTasks.removeAll();
+        self.doingTasks.removeAll();
+        self.doneTasks.removeAll();
+    }
+
     self.isTaskSelected = function (task) {
         return task === self.selectedTask();
     };
 
     self.addTodoTask = function () {
-        var task = new Task("new");
+        var task = new Task(" ");
         self.selectedTask(task);
         self.todoTasks.push(task);
     };
     self.addDoingTask = function () {
-        var task = new Task("new");
+        var task = new Task(" ");
         self.selectedTask(task);
         self.doingTasks.push(task);
     };
     self.addDoneTask = function () {
-        var task = new Task("new");
+        var task = new Task(" ");
         self.selectedTask(task);
         self.doneTasks.push(task);
     };
-    self.save = function () {
-        var todoTasks = ko.toJSON({
-            tasks: self.todoTasks
-        });
-        var doingTasks = ko.toJSON({
-            tasks: self.doingTasks
-        });
-        var doneTasks = ko.toJSON({
-            tasks: self.doneTasks
-        });
-
-        localStorage.setItem(
-            "todoTasks",
-            todoTasks
-        );
-
-        localStorage.setItem(
-            "doingTasks",
-            doingTasks
-        );
-
-        localStorage.setItem(
-            "doneTasks",
-            doneTasks
-        );
-    }
     self.restore = function () {
         var todoTasksJSON = localStorage.getItem("todoTasks");
-        var todoTasks = $.map(JSON.parse(todoTasksJSON).tasks, function (item) {
-            return new Task(item.name)
-        });
-        self.todoTasks(todoTasks);
-
         var doingTasksJSON = localStorage.getItem("doingTasks");
-        var doingTasks = $.map(JSON.parse(doingTasksJSON).tasks, function (item) {
-            return new Task(item.name)
-        });
-        self.doingTasks(doingTasks);
-
         var doneTasksJSON = localStorage.getItem("doneTasks");
-        var doneTasks = $.map(JSON.parse(doneTasksJSON).tasks, function (item) {
-            return new Task(item.name)
-        });
-        self.doneTasks(doneTasks);
+
+        if (todoTasksJSON != null && doingTasksJSON != null && doneTasksJSON != null) {
+            var todoTasks = $.map(JSON.parse(todoTasksJSON).tasks, function (item) {
+                return new Task(item.name)
+            });
+            self.todoTasks(todoTasks);
+
+            var doingTasks = $.map(JSON.parse(doingTasksJSON).tasks, function (item) {
+                return new Task(item.name)
+            });
+            self.doingTasks(doingTasks);
+
+            var doneTasks = $.map(JSON.parse(doneTasksJSON).tasks, function (item) {
+                return new Task(item.name)
+            });
+            self.doneTasks(doneTasks);
+        }
+
+        savingLocked = false;
     }
 
     self.trash = ko.observableArray([]);
@@ -127,7 +157,12 @@ ko.bindingHandlers.visibleAndSelect = {
 
 var localStorage = window.localStorage;
 
-// localStorage.clear();
+function clearLocalStorageAndRefresh() {
+    console.log("asdasdfsdf");
+    localStorage.clear();
+    model.clear();
+}
 
 var model = new ViewModel()
 ko.applyBindings(model);
+model.restore();
